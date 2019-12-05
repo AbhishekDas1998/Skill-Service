8fbfa60ed5ca760393139d5630b0c1a0c1a898d0
AbhishekDas1998 <abhishekdas1998@gmail.com>
2019-12-05 16:07:50 +0530

    Skill Service

---
+++ src/main/java/com/spring/controller/SkillController.java
@@ -0,0 +1,70 @@
+package com.spring.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.spring.dto.Skills;
+import com.spring.service.SkillServiceClass;
+
+@RestController
+@RequestMapping("skill")
+public class SkillController {
+
+	@Autowired
+	SkillServiceClass ser;
+
+	@GetMapping(path = "/getAllSkills")
+	public List<Skills> findAllSkills() {
+		return ser.findAllSkills();
+	}
+
+	@PostMapping(path = "/addSkills")
+	public ResponseEntity<Skills> addSkills(@Valid @RequestBody Skills skill) throws Exception {
+		Skills savedSkills = ser.addSkills(skill);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{sId}")
+				.buildAndExpand(savedSkills.getsId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	@GetMapping(path = "/getSkill/{sId}")
+	public Skills findOneSkill(@PathVariable int sId) {
+		Skills skill = ser.findOneSkill(sId);
+		if (skill.getsId() == 0)
+			throw new SkillNotFoundException("Skill Not found with sId =" + sId);
+		else
+			return skill;
+
+	}
+
+	@DeleteMapping(path = "/deleteSkill/{sId}")
+	public String deleteSkill(@PathVariable int sId) {
+		Skills skill = ser.findOneSkill(sId);
+		if (skill.getsId() == 0) {
+			throw new SkillNotFoundException("Skill Not found with sId =" + sId);
+		} else {
+			ser.deleteSkill(sId);
+			return "Skill deleted with sId "+sId;
+		}
+	}
+
+	@PutMapping(path = "/updateSkill")
+	public String updateSkill(@RequestBody Skills skill) {
+		ser.updateSkill(skill);
+		return "Skill updated with id "+skill.getsId();
+	}
+
+}
---
+++ src/main/java/com/spring/service/SkillServiceClass.java
@@ -0,0 +1,56 @@
+package com.spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.dto.Skills;
+import com.spring.repository.SkillRepository;
+
+
+@Service
+public class SkillServiceClass {
+
+
+	@Autowired
+	SkillRepository repo;
+	
+	public List<Skills> findAllSkills(){
+		return repo.findAll();
+	}
+	
+	public Skills addSkills(Skills skill){
+		  return repo.save(skill);
+	}
+	
+	public Skills findOneSkill(int sId){
+		Optional<Skills> skillOptional = repo.findById(sId);
+		   if(skillOptional.isPresent()) {
+			   return skillOptional.get();
+		   }else {
+			   return new Skills();
+		   }
+	}
+	
+	public void deleteSkill(int sId){
+		
+		repo.deleteById(sId);
+	}
+	
+	public Skills updateSkill(Skills skill)
+	{
+		Skills savedSkills=new Skills();
+		Optional<Skills> skillOptional = repo.findById(skill.getsId());
+		if(skillOptional.isPresent())
+		{
+			savedSkills=skillOptional.get();
+			savedSkills.setsName(skill.getsName());
+			repo.save(savedSkills);
+		}
+		return savedSkills;
+		
+	}
+}
